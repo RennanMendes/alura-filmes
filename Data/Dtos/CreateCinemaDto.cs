5f0d3961e74d979c4e8c145cd075e74d0203f881
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2024-04-14 18:25:01 -0300

    feat: created cinema

---
+++ Data/Dtos/CreateCinemaDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class CreateCinemaDto
+    {
+
+        [Required(ErrorMessage = "O campo de erro é obrigatório!")]
+        public string Nome { get; set; }
+    }
+}
